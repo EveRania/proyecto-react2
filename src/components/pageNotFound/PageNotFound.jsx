73768035de7ba2f17e33fe69d6249e62d6901517
EveRania <ev.r@live.com.ar>
2024-03-30 14:23:50 -0300

    correccion de imagenes

--- src/components/pageNotFound/PageNotFound.jsx
+++ src/components/pageNotFound/PageNotFound.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import img from '../../assets/404.jpg'
+import img from '../../assets/OIP.jpeg'
 import { Image } from '@chakra-ui/react'
 const PageNotFound = () => {
   return (
